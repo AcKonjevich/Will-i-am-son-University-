9c07145c3c07cde2a1f7f5e22ff2a764a46efacf
AcKonjevich <114545694+AcKonjevich@users.noreply.github.com>
2023-03-29 14:54:58 -0400

    Add files via upload

---
+++ ADDCOURSE/AddCourseReg.js
@@ -0,0 +1,39 @@
+
+var dom = document.getElementById("addCourseForm"); 
+//dom.elements[1].onchange = firstNameCheck; //first name. User must enter a Capital Letter, followed by a series of lowercase letters and spaces
+//dom.elements[2].onchange = lastNameCheck;//last name
+//dom.elements[3].onchange = emailCheck;//email
+//dom.elements[4].onclick = otherMethod;//drop down menu
+//dom.elements[5].onclick = checkMajor;//major
+//dom.elements[6].onclick = submitInfo; //button for clicking and submitting the form
+
+
+
+//Semester 
+//dom.elements[1].onchange =
+//Year
+dom.elements[2].onchange = semesterYear;
+//Course Prefix
+dom.elements[3].onchange = coursePget;
+//Course Number
+dom.elements[4].onchange = courseNumber;
+//Section
+dom.elements[5].onchange = courseSection;
+//Course Name
+//dom.elements[6].onchange =
+//Room
+dom.elements[7].onchange = roomCheck;
+//Days Offered
+dom.elements[8].onchange = daysOffered;
+//Time
+//Credit Hours
+dom.elements[12].onchange = creditHours;
+//IFname
+dom.elements[13].onchange = firstNameCheck;
+//ILname
+dom.elements[14].onchange = lastNameCheck;
+//Enrolment Cap
+dom.elements[15].onchange =enrollmentCap;
+dom.elements[16].onclick = submitInfo;
+
+
